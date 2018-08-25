a6409978fbc0253dce1208ce335f7cb208ab8147
martanit <martanit@thinkuntu>
2018-08-25 15:33:03 +0200

    Minor changes to ising_learning.py

--- ising_learning.py
+++ ising_learning.py
@@ -13,10 +13,9 @@ import matplotlib.pyplot as plt
 
 
 
-trans_temp = 2/np.log(1+np.sqrt(2))
-#trans_temp = 4/np.log(3)
 
-def read_data(input_set):
+
+def read_data(input_set, critical_temp):
 
     """Read data from file.
 
@@ -40,7 +39,7 @@ def read_data(input_set):
                 magnetizations.append(float(magnetization))
                 temperature = float(temperature)
                 real_temperatures.append(temperature)
-                if temperature < trans_temp:
+                if temperature < critical_temp:
                     binary_temperatures.append(np.array([1,0]))
                 else:
                     binary_temperatures.append(np.array([0,1]))
@@ -70,9 +69,9 @@ def build_model(data_shape, neurons_number):
             activation=tf.sigmoid,
             kernel_initializer=keras.initializers.RandomNormal(stddev=1),  
             bias_initializer=keras.initializers.RandomNormal(stddev=1),
-            kernel_regularizer=keras.regularizers.l2(0.001),
+            kernel_regularizer=keras.regularizers.l2(0.003),
             input_shape=(data_shape,)),
-        #keras.layers.Dropout(0.2),
+ #       keras.layers.Dropout(0.2),
         keras.layers.Dense(2,
             activation=tf.nn.softmax,
             #kernel_initializer=tf.constant_initializer(np.array([[2, 1, -1], [-2, -2, 1]])),
@@ -81,7 +80,7 @@ def build_model(data_shape, neurons_number):
             bias_initializer=keras.initializers.RandomNormal(stddev=1))
         ])
 
-    optimizer = tf.train.AdamOptimizer()
+    optimizer = tf.train.AdamOptimizer(0.001)
 
     model.compile(loss='binary_crossentropy',
             optimizer=optimizer,
@@ -89,19 +88,23 @@ def build_model(data_shape, neurons_number):
     return model
 
 
+square_temp = 2/np.log(1+np.sqrt(2))
+triangular_temp = 4/np.log(3)
+cubic_temp = 1/0.221654
+
+test_temp = square_temp
 
 train_set = sys.argv[1]
-train_magns, train_bin_temps, train_real_temps, train_configs = read_data(train_set)
+train_magns, train_bin_temps, train_real_temps, train_configs = read_data(train_set, square_temp)
 test_set = sys.argv[2]
-test_magns, test_bin_temps, test_real_temps, test_configs = read_data(test_set)
+test_magns, test_bin_temps, test_real_temps, test_configs = read_data(test_set, test_temp)
 
-neurons_number = 4
+neurons_number = 100
 model = build_model(train_configs.shape[1], neurons_number)
 model.summary()
 
 #Calculate number of training set configurations to give to validation set (80%-20%)
 val_perc = int(train_configs.shape[0]*20/100)
-
 
 config_val = train_configs[:val_perc]
 config_train_part = train_configs[val_perc:]
@@ -111,8 +114,8 @@ temp_train_part = train_bin_temps[val_perc:]
 
 history = model.fit(config_train_part,
         temp_train_part,
-        epochs=100,
-        batch_size=16,
+        epochs=4,
+        batch_size=128,
         validation_data=(config_val, temp_val),
         verbose=1)
 
@@ -123,7 +126,7 @@ print("\nTest loss = " + str(results[0]) + "\nTest accuracy = " + str(results[1]
 # predict label on test dataset
 predictions = model.predict(test_configs)
 
-single_real_temps = np.round_(np.linspace(1.0, 5.0, 41), decimals=2)
+single_real_temps = np.round_(np.linspace(1, 5, 41), decimals=2)
 predictions_t1 = []
 predictions_t2 = []
 
@@ -146,7 +149,7 @@ y1 = predictions_t1[:,0]
 y1_e = predictions_t1[:,1]
 y2 = predictions_t2[:,0]
 y2_e = predictions_t2[:,1]
-plt.axvline(x=trans_temp, marker='|', c='g', label='Trans temp')
+plt.axvline(x=test_temp, marker='|', c='g', label='Trans temp')
 plt.errorbar(x, y1, y1_e, c='b', marker='.', linewidth=2, label='No.1')
 plt.errorbar(x, y2, y2_e, c='r', marker='.', linewidth=2, label='No.2')
 plt.legend()
@@ -155,14 +158,14 @@ plt.show()
 weights = model.layers[0].get_weights()[0]
 bias = model.layers[0].get_weights()[1]
 
-y = np.matmul(test_configs, weights)+bias
-x = test_magns
+#y = np.matmul(test_configs, weights)+bias
+#x = test_magns
 
 
-for i in range(0, neurons_number):
-    plt.scatter(x, y[:,i], c=np.random.rand(3,1), marker='_')
+#for i in range(0, neurons_number):
+ #   plt.scatter(x, y[:,i], c=np.random.rand(3,1), marker='_')
 
-plt.show()
+#plt.show()
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
